1bed928b45c0dce7f56edaf6478dd5103fa65a04
moliver4 <tifftang22@gmail.com>
2020-04-25 16:04:28 -0700

    parsing html

--- scraper.py
+++ scraper.py
@@ -1,7 +1,18 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://www.amazon.com/iRobot-Roomba-675-Connectivity-Carpets/dp/B07DL4QY5V/ref=sr_1_3?dchild=1&keywords=roomba&qid=1587854652&sr=8-3'
 
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
+
+soup1 = BeautifulSoup(page.content, 'html.parser')
+
+soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+
+title = soup2.find(id="productTitle").get_text()
+
+price = soup2.find(id="priceblock_ourprice").get_text()
+
+converted_price = price[0:5]
