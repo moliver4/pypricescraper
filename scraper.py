496a11951162c18fb45d4a3fc0ea19e20ca8ce42
moliver4 <tifftang22@gmail.com>
2020-04-25 16:40:51 -0700

    complete for roomba, dotenv, git ignore added

--- scraper.py
+++ scraper.py
@@ -1,18 +1,55 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+KEY = os.getenv("KEY")
+EMAIL = os.getenv("EMAIL")
 
 URL = 'https://www.amazon.com/iRobot-Roomba-675-Connectivity-Carpets/dp/B07DL4QY5V/ref=sr_1_3?dchild=1&keywords=roomba&qid=1587854652&sr=8-3'
-
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
 
-soup1 = BeautifulSoup(page.content, 'html.parser')
+def check_price(): 
+    page = requests.get(URL, headers=headers)
 
-soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
+    soup1 = BeautifulSoup(page.content, 'html.parser')
 
-title = soup2.find(id="productTitle").get_text()
+    soup2 = BeautifulSoup(soup1.prettify(), 'html.parser')
 
-price = soup2.find(id="priceblock_ourprice").get_text()
+    price = soup2.find(id="priceblock_ourprice").get_text()
 
-converted_price = price[0:5]
+    converted_price = float(price[1:5])
+
+    if(converted_price < 150):
+        send_mail()
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(EMAIL, KEY)
+
+    subject = 'Price dropped!'
+    body = 'https://www.amazon.com/iRobot-Roomba-675-Connectivity-Carpets/dp/B07DL4QY5V/ref=sr_1_3?dchild=1&keywords=roomba&qid=1587854652&sr=8-3'
+
+
+    message = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        EMAIL,
+        EMAIL,
+        message
+    )
+
+    print('SENT EMAIL')
+
+    server.quit()
+
+
+check_price()
